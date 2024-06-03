f17a99a3aee4981b70775de9c2eaea35beb3f883
AugustoCantero <augustocantero2002@gmail.com>
2024-06-03 20:47:50 -0300

    entrega

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-3">
+    <div class="jumbotron">
+        <h1>Parcial</h1>
+        <br>
+
+
+        <h1></h1>
+      <form @submit.prevent>
+        <label for="texto"></label>
+        <input v-model="texto" id="texto" type="text" />
+      </form>
+
+      <div>
+        <h3>Cantidad de caracteres: {{ cantidadCaracteres }}</h3>
+      </div>
+
+      <div v-if="texto">
+        <h2></h2>
+        <ul>
+          <li><strong>Codificado:</strong> {{ codificarTexto(texto) }}</li>
+          <li><strong>Todo en mayúscula:</strong> {{ texto.toUpperCase() }}</li>
+          <li><strong>Todo en minúscula:</strong> {{ texto.toLowerCase() }}</li>
+          <li><strong>Intercalado (mayúscula primero):</strong> {{ alternarTexto(texto, true) }}</li>
+          <li><strong>Intercalado (minúscula primero):</strong> {{ alternarTexto(texto, false) }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <p>respuestas: 1:C 2:C 3:A </p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      texto: ''
+    };
+  },
+  computed: {
+    cantidadCaracteres() {
+      return this.texto.length;
+    }
+  },
+  methods: {
+    codificarTexto(texto) {
+      const vocales = {
+        'a': 'u',
+        'e': 'o',
+        'i': 'i',
+        'o': 'e',
+        'u': 'a'
+      };
+      return texto.toLowerCase().split('').map(char => vocales[char] || char).join('');
+    },
+    alternarTexto(texto, mayusculaInicial) {
+      return texto.split('').map((char, index) => {
+        if (mayusculaInicial) {
+          return index % 2 === 0 ? char.toUpperCase() : char.toLowerCase();
+        } else {
+          return index % 2 === 0 ? char.toLowerCase() : char.toUpperCase();
+        }
+      }).join('');
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  h1 {
+    color: crimson;
+  }
+  h2 {
+    color: blue;
+  }
 </style>
